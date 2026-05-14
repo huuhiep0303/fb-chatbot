370d97b50c28044c9668e34c7440081bcc502e0d
Hiep Cao Huu <cacaohh04@gmail.com>
2026-05-14 20:28:32 +0700

    add bot reply

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 
 async function sendMessage(senderId, text) {
+  console.log("BOT:", text); 
+  
   try {
     await axios.post(
       "https://graph.facebook.com/v18.0/me/messages",
